7bf8ca18f502df5bcde7e3c26c2d10a8fed066d5
Automation SandBox <automationsandbox@samis-MacBook-Pro.local>
2022-10-01 18:06:41 -0400

    Initial commit

---
+++ src/test/java/Tests/TestFunc.java
@@ -0,0 +1,13 @@
+package Tests;
+
+public class TestFunc {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("whats up, welcome to the api json automation course");
+
+    }
+
+
+}
